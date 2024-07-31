e291621383dfd7148ccbf7531aefcfd7a2ab151c
scionsamurai <scionsamurai@yahoo.com>
2024-07-31 06:41:35 -0500

    trying to get www subdomain working

--- svelte.config.js
+++ svelte.config.js
@@ -33,19 +33,7 @@ const config = {
 	},
 	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 	kit: {
-		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			},
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
-			}
-		})
+		adapter: adapter()
 	}
 }
 
