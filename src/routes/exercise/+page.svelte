d3ea51e10b2d2fb88a0ef59d666b668f2bdc5757
scionsamurai <21150520+scionsamurai@users.noreply.github.com>
2025-08-19 15:41:28 -0500

    another test

--- src/routes/exercise/+page.svelte
+++ src/routes/exercise/+page.svelte
@@ -8,7 +8,7 @@
 	<title>{config.siteName} - terms of service</title>
 </svelte:head>
 
-<iframe src="https://claude.site/public/artifacts/ae995591-1ed4-4a01-9ff2-5c6ef47ab163/embed" title="Claude Artifact" width="100%" height="80vh" frameborder="0" allow="clipboard-write" allowfullscreen></iframe>
+<iframe src="https://claude.site/public/artifacts/ae995591-1ed4-4a01-9ff2-5c6ef47ab163/embed" title="Claude Artifact" width="100%" height="800px" frameborder="0" allow="clipboard-write" allowfullscreen></iframe>
 
 <div role="main" class="singlePage">
 	<svelte:component this={data.content} slug={data.slug} />
